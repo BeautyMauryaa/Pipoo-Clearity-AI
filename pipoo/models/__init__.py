a0801e210af7943e38ab355138e19d74b545040d
Nova <beautymaurya9142@gmail.com>
2026-04-03 00:00:25 +0530

    Delete pipoo directory

--- pipoo/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data Models Package
-"""
-from models.user import User
-from models.note import Note
-from models.reminder import Reminder
-from models.chat import ChatMessage
-
-__all__ = ['User', 'Note', 'Reminder', 'ChatMessage']
